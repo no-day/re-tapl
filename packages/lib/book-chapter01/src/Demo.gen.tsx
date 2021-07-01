a5e7a3c21ba560ace57c3afb84a85df760d05fd5
Michael Bock <me@thought2.de>
2021-07-01 09:29:31 +0200

    Init

---
+++ packages/lib/book-chapter01/src/Demo.gen.tsx
@@ -0,0 +1,13 @@
+/* TypeScript file generated from Demo.res by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// @ts-ignore: Implicit any on import
+const DemoBS = require('./Demo.bs');
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = {};
+
+export const make: React.ComponentType<{}> = DemoBS.make;
